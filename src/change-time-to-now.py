fec3dab580de54e18f2da2b86517fec525ada083
Dov <dovscode@icloud.com>
2018-10-17 10:03:41 +1100

    core functionality

---
+++ src/change-time-to-now.py
@@ -0,0 +1,33 @@
+import sys
+import random
+
+def _success():
+	print('INFO: change time to now completed successfully!')
+	print('INFO: time IS now')
+
+def _failure():
+	print('ERR: change time to now failed')
+	print('ERR: time is NOT now!!')
+
+def _warning():
+	import traceback
+	
+	try:
+		raise Exception('Unknown exception caught')
+	except:
+		traceback.print_exc()
+		print('ERR: change time to now failed unexpectedly')
+		print('ERR: Warning: Time might not be now!!')
+
+def _welcome():
+	print('Change time to now v0.0.1-10')
+	print('')
+
+def main():
+	_welcome()
+	funcs = (_success, _failure, _warning)
+
+	random.choice(funcs)()
+
+if __name__ == '__main__':
+	sys.exit(main())
