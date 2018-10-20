c853e43ca3b7ab4b5df72baff938e19e1d5ce1b5
Dov <dovscode@icloud.com>
2018-10-20 13:49:27 +1100

    run docker interactive

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
 make > /dev/null
-docker run --rm --name ctn dovdor/change-time-to-now:latest "$@"
+docker run --rm --name ctn -it dovdor/change-time-to-now:latest "$@"
