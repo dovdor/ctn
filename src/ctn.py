621cb79529e45d0990d3130f4fb7da19f4abf85f
Dov <dovscode@icloud.com>
2018-10-20 12:13:50 +1100

    tabs are now spaces

--- src/ctn.py
+++ src/ctn.py
@@ -2,27 +2,27 @@ import sys
 import random
 
 def _success():
-	print('INFO: change time to now completed successfully!')
-	print('INFO: time IS now')
+    print('INFO: change time to now completed successfully!')
+    print('INFO: time IS now')
 
 def _failure():
-	print('ERR: change time to now failed')
-	print('ERR: time is NOT now!!')
+    print('ERR: change time to now failed')
+    print('ERR: time is NOT now!!')
 
 def _warning():
-	import traceback
-	
-	try:
-		raise Exception('Unknown exception caught')
-	except:
-		traceback.print_exc()
-		print('ERR: change time to now failed unexpectedly')
-		print('ERR: Warning: Time might not be now!!')
+    import traceback
+
+    try:
+        raise Exception('Unknown exception caught')
+    except:
+        traceback.print_exc()
+        print('ERR: change time to now failed unexpectedly')
+        print('ERR: Warning: Time might not be now!!')
 
 def main():
-	funcs = (_success, _failure, _warning)
+    funcs = (_success, _failure, _warning)
 
-	random.choice(funcs)()
+    random.choice(funcs)()
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
